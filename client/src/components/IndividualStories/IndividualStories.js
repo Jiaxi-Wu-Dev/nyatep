c44920db2402353eea50d2d79a64853adf5769ec
Jiaxi <dev.jiaxiwu@gmail.com>
2021-04-15 22:18:48 -0400

    Created individualstories component

---
+++ client/src/components/IndividualStories/IndividualStories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./individualStories.css";
+
+const IndividualStories = () => {
+  return (
+    <div className="individualStories__container">
+      <div className="individualStories__navbar">
+        <a class="active" href="#home">Home</a>
+        <a href="#counties">Counties</a>
+        <a href="#link">link</a>
+        <a href="#link">link</a>
+      </div>
+    </div>
+  );
+};
+
+export default IndividualStories;
